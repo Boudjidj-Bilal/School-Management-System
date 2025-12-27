0a69857baabb9ebb2b9575efbb2e44906b3718a3
Boudjidj-Bilal <bilelboudjidj8@gmail.com>
2025-12-27 12:26:09 +0100

    ajout de mes pages js pour talwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      'templates/*.html',
+      './templates/*.html',
       './**/templates/**/*.html',
       './**/forms.py', 
+      './static/js/**/*.js',
   ],
   theme: {
     extend: {},
