ad012c3364e2bbf400d27a26214fc304f433eee0
Boudjidj-Bilal <bilelboudjidj8@gmail.com>
2026-01-19 19:46:33 +0100

    prod fini

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      './templates/*.html',
-      './**/templates/**/*.html',
-      './**/forms.py', 
-      './static/js/**/*.js',
-      './static/js/*.js',
+    "./templates/**/*.html",
+    "./**/templates/**/*.html",
+    "./**/*.html",
+    "./**/*.js",
+    "./**/*.py",
   ],
   theme: {
     extend: {
-      // AJOUT DES COULEURS PERSONNALISÉES ICI
       colors: {
-        'primary-blue': '#1e40af',  // Le bleu foncé/violet que tu utilisais
-        'primary-light': '#eff6ff', // Le fond bleu très clair
-        'accent-green': '#10b981',  // Le vert pour les messages de succès
+        'primary-blue': '#1e40af',
+        'primary-light': '#eff6ff',
+        'accent-green': '#10b981',
       }
     },
   },
